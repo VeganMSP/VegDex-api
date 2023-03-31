6cf3d5974e0dab528570eb042d9bff943a0acdaa
Matthew Jorgensen <matthew@veganmsp.com>
2023-03-30 23:42:00 -0500

    C# Rewrite - Remove Python (#37)

--- api/config/settings/local.py
+++
@@ -1,6 +0,0 @@
-from .common import *  # noqa
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=y-u(pv5)gxli$#8n!zkb+7g=op!1*f45h_4z+d$4$ot3w#vuy'
-
-DEBUG = env.bool("DJANGO_DEBUG", default=True)
--- api/vegdex_api/shopping/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import (
-    FarmersMarket,
-    VeganCompany,
-)
-
-admin.site.register(FarmersMarket)
-admin.site.register(VeganCompany)
