53d7ba2e220be9f1d28c99098e04c3f3057274e2
prplecake <matthew@veganmsp.com>
2023-03-31 07:57:17 -0500

    Add data protection backed by EF

---
+++ src/VegDex.Infrastructure/Context/AppKeysContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VegDex.Infrastructure.Context;
+
+public class AppKeysContext : DbContext, IDataProtectionKeyContext
+{
+    
+    public AppKeysContext(DbContextOptions<AppKeysContext> options) : base(options) { }
+    /// <inheritdoc />
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+}
