b601fc358bd85a741b1e0a4c563aaeae2166bf50
Matthew Jorgensen <matthew@veganmsp.com>
2023-03-31 01:40:26 -0500

    Only use hsts if we're allowing https redirection and aren't in development

--- VegDex.Web.MVC/Program.cs
+++ VegDex.Web.MVC/Program.cs
@@ -21,13 +21,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 if (builder.Configuration.GetValue<bool>("HttpRedirection"))
 {
     app.UseHttpsRedirection();
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 }
 app.UseStaticFiles();
 
