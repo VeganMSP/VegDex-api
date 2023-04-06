603bc9725336fa4e410e5d4ca2177f30c47e7452
Matthew Jorgensen <matthew@veganmsp.com>
2023-04-05 21:14:58 -0500

    Remove get/set override

--- src/VegDex.Web.MVC/ViewModels/Meta/AboutPageViewModel.cs
+++ src/VegDex.Web.MVC/ViewModels/Meta/AboutPageViewModel.cs
@@ -1,14 +1,7 @@
-using Markdig;
-
 namespace VegDex.Web.MVC.ViewModels.Meta;
 
 public class AboutPageViewModel
 {
-    private string _content;
-    public string Content
-    {
-        get => Markdown.ToHtml(_content);
-        set => _content = value;
-    }
+    public string Content { get; set; }
     public DateTime DateUpdated { get; set; }
 }
