e954c85f6a2df6b90bd959a68258218171d1a2ba
Matthew Jorgensen <matthew@veganmsp.com>
2023-04-06 20:14:06 -0500

    Finish CityService methods

--- src/VegDex.Application/Services/CityService.cs
+++ src/VegDex.Application/Services/CityService.cs
@@ -2,6 +2,7 @@ using Serilog;
 using VegDex.Application.Interfaces;
 using VegDex.Application.Mapper;
 using VegDex.Application.Models;
+using VegDex.Core.Entities;
 using VegDex.Core.Repositories;
 
 namespace VegDex.Application.Services;
@@ -29,9 +30,31 @@ public class CityService : ICityService
         return mapped;
     }
     /// <inheritdoc />
-    public async Task<CityModel> Create(CityModel city) => throw new NotImplementedException();
+    public async Task<CityModel> Create(CityModel cityModel)
+    {
+        await ValidateCityIfExist(cityModel);
+
+        var mappedEntity = ObjectMapper.Mapper.Map<City>(cityModel);
+        if (mappedEntity == null)
+            throw new ApplicationException("Entity could not be mapped");
+
+        var newEntity = await _cityRepository.AddAsync(mappedEntity);
+        _logger.Information("Entity successfully added: {@City}", newEntity);
+
+        var newMappedEntity = ObjectMapper.Mapper.Map<CityModel>(newEntity);
+        return newMappedEntity;
+    }
     /// <inheritdoc />
-    public async Task Update(CityModel city) => throw new NotImplementedException();
+    public async Task Update(CityModel cityModel)
+    {
+        ValidateCityIfNotExist(cityModel);
+        var editCity = await _cityRepository.GetByIdAsync(cityModel.Id);
+        if (editCity == null)
+            throw new ApplicationException("Entity could not be loaded.");
+        ObjectMapper.Mapper.Map(cityModel, editCity);
+        await _cityRepository.UpdateAsync(editCity);
+        _logger.Information("Entity successfully updated");
+    }
     /// <inheritdoc />
     public async Task<CityModel> GetCityByName(string cityName)
     {
@@ -40,12 +63,32 @@ public class CityService : ICityService
         return mapped;
     }
     /// <inheritdoc />
-    public async Task Delete(CityModel city) => throw new NotImplementedException();
+    public async Task Delete(CityModel cityModel)
+    {
+        ValidateCityIfNotExist(cityModel);
+        var deletedCity = await _cityRepository.GetByIdAsync(cityModel.Id);
+        if (deletedCity == null)
+            throw new ApplicationException("Entity could not be loaded.");
+        await _cityRepository.DeleteAsync(deletedCity);
+        _logger.Information("Entity successfully deleted");
+    }
     /// <inheritdoc />
     public async Task<IEnumerable<CityModel>> GetCitiesWithRestaurants()
     {
         var citiesWithRestaurants = await _cityRepository.GetCitiesWithRestaurants();
         var mapped = ObjectMapper.Mapper.Map<IEnumerable<CityModel>>(citiesWithRestaurants);
         return mapped;
+    }
+    async private Task ValidateCityIfExist(CityModel cityModel)
+    {
+        var existingEntity = await _cityRepository.GetByIdAsync(cityModel.Id);
+        if (existingEntity != null)
+            throw new ApplicationException($"{cityModel} with this Id exists already");
+    }
+    private void ValidateCityIfNotExist(CityModel cityModel)
+    {
+        var existingEntity = _cityRepository.GetByIdAsync(cityModel.Id);
+        if (existingEntity == null)
+            throw new ApplicationException($"{cityModel} with this Id does not exist");
     }
 }
