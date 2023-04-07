6c2cd9f1eaca0141d5f465aac1e255ac19dd66b6
Matthew Jorgensen <matthew@veganmsp.com>
2023-04-06 20:34:57 -0500

    Update City/Index

--- src/VegDex.Application/Interfaces/IRestaurantService.cs
+++ src/VegDex.Application/Interfaces/IRestaurantService.cs
@@ -7,7 +7,7 @@ public interface IRestaurantService
     Task<RestaurantModel> Create(RestaurantModel restaurant);
     Task Delete(RestaurantModel restaurant);
     Task<RestaurantModel> GetRestaurantById(int? restaurantId);
-    Task<IEnumerable<RestaurantModel>> GetRestaurantByLocation(int locationId);
+    Task<IEnumerable<RestaurantModel>> GetRestaurantsByLocation(int locationId);
     Task<RestaurantModel> GetRestaurantByName(string? restaurantName);
     Task<IEnumerable<RestaurantModel>> GetRestaurantList();
     Task Update(RestaurantModel restaurant);
