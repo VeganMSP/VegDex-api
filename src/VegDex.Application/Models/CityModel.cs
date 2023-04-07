385ee0e0aa3a9a3ad547f7c1e6a1964021c16817
Matthew Jorgensen <matthew@veganmsp.com>
2023-04-06 20:13:35 -0500

    Update CityModel

--- src/VegDex.Application/Models/CityModel.cs
+++ src/VegDex.Application/Models/CityModel.cs
@@ -5,9 +5,9 @@ namespace VegDex.Application.Models;
 
 public class CityModel : BaseModel
 {
-    [Required] public string Name { get; set; }
-    public IEnumerable<RestaurantModel> Restaurants { get; set; }
-    public string Slug { get; set; }
+    public string Name { get; set; }
+    public IEnumerable<RestaurantModel>? Restaurants { get; set; }
+    public string? Slug { get; set; }
     /// <inheritdoc />
     public override string ToString() => Name;
 }
