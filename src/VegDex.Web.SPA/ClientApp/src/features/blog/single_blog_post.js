3c118e4fd022e2d38c06863acd378832389747e3
prplecake <matthew@veganmsp.com>
2023-07-05 13:18:46 -0500

    Initialize react app

---
+++ src/VegDex.Web.SPA/ClientApp/src/features/blog/single_blog_post.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import {format} from "date-fns";
+import withRouter from '../../helpers/withRouter';
+
+class SingleBlogPost extends Component {
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+    slug: PropTypes.string,
+    params: PropTypes.object,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.params.slug,
+      blog_post: {},
+      loading: true
+    }
+  }
+
+  static renderBlogPost(blog_post) {
+    const {title, content, created_at} = blog_post
+
+    return (
+      <div className='post-stub'>
+        <h3 className='post-title'>
+          <span className='date xs-hidden'>
+            {format(new Date(created_at), 'yyyy-MM-dd')}
+            &nbsp;
+            {title}
+          </span>
+        </h3>
+        <p className='post-content'>{content}</p>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.getBlogPost(this.state.slug)
+  }
+
+  render() {
+    let content = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : SingleBlogPost.renderBlogPost(this.state.blog_post);
+    return (
+      <>
+        {content}
+      </>
+    );
+  }
+
+  async getBlogPost(slug) {
+    const reponse = await fetch('/api/v1/blog_posts?' + new URLSearchParams({
+      slug: slug
+    }));
+    const data = await reponse.json();
+    this.setState({blog_post: data, loading: false});
+  }
+}
+
+export default withRouter(SingleBlogPost);
