234a8d8eb194779629411594e6eb9d3af37ff49f
prplecake <matthew@veganmsp.com>
2023-07-05 22:28:09 -0500

    Remove empty constructors

--- src/VegDex.Core/Entities/VeganCompany.cs
+++ src/VegDex.Core/Entities/VeganCompany.cs
@@ -2,7 +2,6 @@ namespace VegDex.Core.Entities;
 
 public class VeganCompany : Entity
 {
-    public VeganCompany() { }
     public string? Description { get; set; }
     public string Name { get; set; }
     public string Slug { get; set; }
