086e44ea5e57944c442bf9075bdf39d2d68da69a
prplecake <matthew@veganmsp.com>
2023-09-30 12:30:48 -0500

    Configure JsonOptions, ignore cycles

---
+++ src/VegDex.Web.API/Setup/ConfigureJsonOptions.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace VegDex.Web.API.Setup;
+
+public class ConfigureJsonOptions : IConfigureOptions<JsonOptions>
+{
+    private static readonly ILogger _logger = Log.ForContext<ConfigureJsonOptions>();
+    /// <inheritdoc />
+    public void Configure(JsonOptions options)
+    {
+        _logger.Debug("Configuring JsonOptions...");
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    }
+}
